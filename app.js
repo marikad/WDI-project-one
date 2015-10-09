f1138bdd405d33d390917299dfc2ef4ab1d08632
webdev11 <marikadevan@gmail.com>
2015-10-09 12:42:33 +0100

    added the click event to the letter divs

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
+
+window.onload = function() {
+
+
 var words = ['cat', 'dog', 'lizard', 'bunny', 'super'];
 var lettersGuessed = []
 var secretWord  = words[Math.floor(Math.random() * words.length)];
+var letters = $('.letters')
 
-console.log(secretWord)
+
+/*for(var i = 0; i < letters.length; i++){
+	letters[i].addEventListener('click', chooseWord);
+}*/
+    $(function() { 
+	$('.letters').on('click', function() {
+	 console.log('i\'ve been clicked')
+
+	})
+
+
+	function chooseWord(){
+   
+
+	}
+
+
+	})
+
+
+
+
+}
