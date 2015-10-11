3efeba3e4cbdd15ba3de1627cee010f2d13ab7a0
webdev11 <marikadevan@gmail.com>
2015-10-11 12:24:30 +0100

    I added the background image to the photo ul. I added a min-height to make it fit and appear as thr child divs dissappear.

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ function setupEvents() {
 function chooseWord() {
     word = $('input').val();
     $('#chosen-word').html(word);
+    $('.user-input').hide("slow");
     // Setup spaces
     var wordArray = word.split("");
     console.log(wordArray)
@@ -23,6 +24,7 @@ function chooseWord() {
         $('.chosen-word').append("<li></li>");
         document.getElementById("number-of-letters").innerHTML =
             "There are " + word.length + " letters in this word";
+
     })
 }
 
@@ -31,7 +33,9 @@ function chooseLetter() {
     lettersGuessed.push(letter)
     checkLetter(letter)
     console.log(lettersGuessed);
-}
+  }
+    
+
 
 function checkLetter(letter) {
     var index = word.indexOf(letter);
@@ -41,7 +45,7 @@ function checkLetter(letter) {
             // Fill in the letter
         $($('.chosen-word li')[index]).html(letter)
     } else {
-        $('#row-image1').show("slow").hide("slow");
+        $('#row-image1').css("visibility", "hidden");
         //$('#row-image2').show("slow").hide("slow");
     }
 }
